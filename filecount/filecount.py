d48b11f25a7fa0e3c80699eb6cb250d5c8ef9dd5
sktnkysh <sktnkysh+dev@gmail.com>
2018-02-01 18:58:05 +0900

    Add filecount

--- filecount/filecount.py
+++ filecount/filecount.py
@@ -1,21 +1,15 @@
 import os
 import shutil
 
+
 def main():
     import argparse
     parser = argparse.ArgumentParser(description='count files')
-    parser.add_argument('dir', default='.')
-    parser.add_argument('-r', dest='recursive', action='store_true')
-    parser.add_argument('-d', dest='onlydir', action='store_true')
-    parser.add_argument('-f', dest='onlyfile', action='store_true')
+    parser.add_argument('directory', default='.')
     args = parser.parse_args()
 
-    if args.recursive:
-        for d in os.listdir(args.dir):
-            dir = os.path.join(args.dir, d)
-            if os.path.isdir(dir):
-                res = len(os.listdir(dir))
-                print('{}\t{}'.format(dir, res))
-
-    res = len(os.listdir(args.dir))
-    print('{}\t{}'.format('.', res))
+    target_dir = args.directory
+    for d in next(os.walk(target_dir))[1]:
+        dir = os.path.join(target_dir, d)
+        n_files = len(os.listdir(dir))
+        print('{}\t{}'.format(dir, n_files))
